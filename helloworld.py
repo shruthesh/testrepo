06bc63f909c273dd8553eab6736f614d490265d4
shruthesh <66586815+shruthesh@users.noreply.github.com>
2025-01-21 21:37:03 +0530

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,4 @@
+Python 3.13.1 (v3.13.1:06714517797, Dec  3 2024, 14:00:22) [Clang 15.0.0 (clang-1500.3.9.4)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("Hello World")
+Hello World
